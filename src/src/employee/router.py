7506541ecd0cc0e0814adb69dd71b7879490420e
W1nLin4n <grechkaartema@gmail.com>
2024-04-21 23:46:28 +0300

    Moved routers code to newly created modules without changes

---
+++ src/src/employee/router.py
@@ -0,0 +1,121 @@
+from typing import Dict, List  # noqa: F401
+
+from openapi_server.apis.employee_api_base import BaseEmployeeApi
+
+from fastapi import (  # noqa: F401
+    APIRouter,
+    Body,
+    Cookie,
+    Depends,
+    Form,
+    Header,
+    Path,
+    Query,
+    Response,
+    Security,
+    status,
+)
+
+from openapi_server.models.extra_models import TokenModel  # noqa: F401
+from openapi_server.models.employee import Employee
+from openapi_server.models.employee_criteria import EmployeeCriteria
+from openapi_server.models.employee_list_response import EmployeeListResponse
+from openapi_server.models.employee_view import EmployeeView
+
+
+router = APIRouter()
+
+@router.put(
+    "/api/employee",
+    responses={
+        200: {"model": int, "description": "Id of new employee"},
+    },
+    tags=["employee"],
+    summary="Create a new employee",
+    response_model_by_alias=True,
+)
+async def create_employee(
+    employee_view: EmployeeView = Body(None, description=""),
+) -> int:
+    """Create a new employee"""
+    return BaseEmployeeApi.subclasses[0]().create_employee(employee_view)
+
+
+@router.delete(
+    "/api/employee/{id}",
+    responses={
+        200: {"model": bool, "description": "Boolean whether employee was deleted"},
+    },
+    tags=["employee"],
+    summary="Delete an employee by id",
+    response_model_by_alias=True,
+)
+async def delete_employee(
+    id: int = Path(..., description=""),
+) -> bool:
+    """Delete an employee by id"""
+    return BaseEmployeeApi.subclasses[0]().delete_employee(id)
+
+
+@router.get(
+    "/api/employee/{id}",
+    responses={
+        200: {"model": Employee, "description": "Employee by id"},
+    },
+    tags=["employee"],
+    summary="Get employee by id",
+    response_model_by_alias=True,
+)
+async def get_employee_by_id(
+    id: int = Path(..., description=""),
+) -> Employee:
+    """Get employee by id"""
+    return BaseEmployeeApi.subclasses[0]().get_employee_by_id(id)
+
+
+@router.post(
+    "/api/employee",
+    responses={
+        200: {"model": EmployeeListResponse, "description": "List of employees"},
+    },
+    tags=["employee"],
+    summary="Get list of employees",
+    response_model_by_alias=True,
+)
+async def get_employee_list(
+    employee_criteria: EmployeeCriteria = Body(None, description=""),
+) -> EmployeeListResponse:
+    """Get list of employees"""
+    return BaseEmployeeApi.subclasses[0]().get_employee_list(employee_criteria)
+
+
+@router.get(
+    "/api/employee/me",
+    responses={
+        200: {"model": int, "description": "Employee id of self"},
+    },
+    tags=["employee"],
+    summary="Get employee id of self",
+    response_model_by_alias=True,
+)
+async def get_employee_me(
+) -> int:
+    """Get employee id of self"""
+    return BaseEmployeeApi.subclasses[0]().get_employee_me()
+
+
+@router.post(
+    "/api/employee/{id}",
+    responses={
+        200: {"model": bool, "description": "Boolean whether employee was updated"},
+    },
+    tags=["employee"],
+    summary="Update existing employee",
+    response_model_by_alias=True,
+)
+async def update_employee(
+    id: int = Path(..., description=""),
+    employee_view: EmployeeView = Body(None, description=""),
+) -> bool:
+    """Update existing employee"""
+    return BaseEmployeeApi.subclasses[0]().update_employee(id, employee_view)
---
+++ src/src/product/router.py
@@ -0,0 +1,107 @@
+from typing import Dict, List  # noqa: F401
+
+from openapi_server.apis.product_api_base import BaseProductApi
+
+from fastapi import (  # noqa: F401
+    APIRouter,
+    Body,
+    Cookie,
+    Depends,
+    Form,
+    Header,
+    Path,
+    Query,
+    Response,
+    Security,
+    status,
+)
+
+from openapi_server.models.extra_models import TokenModel  # noqa: F401
+from openapi_server.models.product import Product
+from openapi_server.models.product_criteria import ProductCriteria
+from openapi_server.models.product_list_response import ProductListResponse
+from openapi_server.models.product_view import ProductView
+
+
+router = APIRouter()
+
+
+@router.put(
+    "/api/product",
+    responses={
+        200: {"model": int, "description": "Id of new product"},
+    },
+    tags=["product"],
+    summary="Create a new product",
+    response_model_by_alias=True,
+)
+async def create_product(
+    product_view: ProductView = Body(None, description=""),
+) -> int:
+    """Create a new product"""
+    return BaseProductApi.subclasses[0]().create_product(product_view)
+
+
+@router.delete(
+    "/api/product/{id}",
+    responses={
+        200: {"model": bool, "description": "Boolean whether product was deleted"},
+    },
+    tags=["product"],
+    summary="Delete a product by id",
+    response_model_by_alias=True,
+)
+async def delete_product(
+    id: int = Path(..., description=""),
+) -> bool:
+    """Delete a product by id"""
+    return BaseProductApi.subclasses[0]().delete_product(id)
+
+
+@router.get(
+    "/api/product/{id}",
+    responses={
+        200: {"model": Product, "description": "product by id"},
+    },
+    tags=["product"],
+    summary="Get product by id",
+    response_model_by_alias=True,
+)
+async def get_product_by_id(
+    id: int = Path(..., description=""),
+) -> Product:
+    """Get product by id"""
+    return BaseProductApi.subclasses[0]().get_product_by_id(id)
+
+
+@router.post(
+    "/api/product",
+    responses={
+        200: {"model": ProductListResponse, "description": "List of products"},
+    },
+    tags=["product"],
+    summary="Get list of products",
+    response_model_by_alias=True,
+)
+async def get_product_list(
+    product_criteria: ProductCriteria = Body(None, description=""),
+) -> ProductListResponse:
+    """Get list of products"""
+    return BaseProductApi.subclasses[0]().get_product_list(product_criteria)
+
+
+@router.post(
+    "/api/product/{id}",
+    responses={
+        200: {"model": bool, "description": "Boolean whether product was updated"},
+    },
+    tags=["product"],
+    summary="Update existing product",
+    response_model_by_alias=True,
+)
+async def update_prodact(
+    id: int = Path(..., description=""),
+    product_view: ProductView = Body(None, description=""),
+) -> bool:
+    """Update existing product"""
+    return BaseProductApi.subclasses[0]().update_prodact(id, product_view)
