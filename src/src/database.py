57cf8a2f33245da814bb6ed6f34cdd818b65bc1d
W1nLin4n <grechkaartema@gmail.com>
2024-04-25 22:20:15 +0300

    Fixed database connection pool annotation

--- src/src/database.py
+++ src/src/database.py
@@ -6,8 +6,7 @@ pool = AsyncConnectionPool(DB_URL, open=False)
 
 
 def db_conn(f):
-    def db_conn_():
+    async def db_conn_():
         async with pool.connection() as conn:
-            f(conn=conn)
-
+            return await f(conn=conn)
     return db_conn_
