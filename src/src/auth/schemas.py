5a42140f33f585c2d84bacbad6b48fde3dd84f46
W1nLin4n <grechkaartema@gmail.com>
2024-04-26 22:30:41 +0300

    Added schemas for authentication

---
+++ src/src/auth/schemas.py
@@ -0,0 +1,44 @@
+from enum import Enum
+from typing import Annotated
+
+from pydantic import BaseModel, Field
+
+from schemas import ListResponse, BaseCriteria
+
+
+class UserRole(str, Enum):
+    CASHIER = "CASHIER"
+    MANAGER = "MANAGER"
+    ADMIN = "ADMIN"
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    login: str | None = None
+    scopes: list[str] = []
+
+
+class UserBase(BaseModel):
+    login: str
+    role_id: Annotated[int, Field(alias="roleId")]
+    employee_id: Annotated[int | None, Field(alias="employeeId")] = None
+
+
+class UserCriteria(BaseCriteria):
+    ids: list[str] | None = None
+
+
+class UserUpsertRequest(UserBase):
+    password: str
+
+
+class UserResponse(UserBase):
+    pass
+
+
+class UserListResponse(ListResponse):
+    items: list[UserResponse]
