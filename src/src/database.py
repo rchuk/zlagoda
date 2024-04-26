5663b397ec12cbc53ccd58298d376646610b67d1
W1nLin4n <grechkaartema@gmail.com>
2024-04-26 03:28:17 +0300

    Fixed db connection decorator

--- src/src/database.py
+++ src/src/database.py
@@ -6,7 +6,7 @@ pool = AsyncConnectionPool(DB_URL, open=False)
 
 
 def db_conn(f):
-    async def db_conn_():
+    async def db_conn_(*args, **kwargs):
         async with pool.connection() as conn:
-            return await f(conn=conn)
+            return await f(*args, **kwargs, conn=conn)
     return db_conn_
