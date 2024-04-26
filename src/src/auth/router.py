9d2182677405b99fb3cab815e1458f2ae2de0f9f
W1nLin4n <grechkaartema@gmail.com>
2024-04-26 22:35:57 +0300

    Added routing for authentication services

---
+++ src/src/auth/router.py
@@ -0,0 +1,29 @@
+from datetime import timedelta
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+
+from auth import service
+from auth.config import ACCESS_TOKEN_EXPIRE_TIME
+from auth.exception import CredentialsError
+from auth.schemas import Token
+from auth.utils import create_access_token
+
+router = APIRouter()
+
+
+@router.post("/api/token")
+async def login_for_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    user = await service.authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise CredentialsError()
+    scopes = [await service.get_role(user.role_id)]
+    access_token_expires = timedelta(ACCESS_TOKEN_EXPIRE_TIME)
+    access_token = await create_access_token(
+        data={"sub": user.login, "scopes": scopes},
+        expires_delta=access_token_expires,
+    )
+    return Token(access_token=access_token, token_type="bearer")
